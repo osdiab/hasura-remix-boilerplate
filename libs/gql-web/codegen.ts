890a16ac43b1aab9ec3cd77de229892502002857
Omar Diab <me@omardiab.com>
2023-02-12 14:24:42 +0900

    Add rome to project

--- libs/gql-web/codegen.ts
+++ libs/gql-web/codegen.ts
@@ -1,28 +1,27 @@
-
-import type { CodegenConfig } from '@graphql-codegen/cli';
+import type { CodegenConfig } from "@graphql-codegen/cli";
 
 const config: CodegenConfig = {
-  overwrite: true,
-  schema: "http://localhost:8002/v1/graphql",
-  documents: ["../../apps/web-app/app/**/*.{ts,tsx}"],
-  ignoreNoDocuments: true,
-  generates: {
-    "generated/": {
-      preset: "client",
-      plugins: [],
-      config: {
-        useTypeImports: true,
-        scalars: {
-          "uuid": "string"
-        },
-      },
-      presetConfig: {
-        // recommended by graphql-codegen since it's not actually a react hook
-        // https://the-guild.dev/graphql/codegen/plugins/presets/preset-client#the-usefragment-helper
-        fragmentMasking: { unmaskFunctionName: 'getFragmentData' }
-      },
-    },
-  }
+	overwrite: true,
+	schema: "http://localhost:8002/v1/graphql",
+	documents: ["../../apps/web-app/app/**/*.{ts,tsx}"],
+	ignoreNoDocuments: true,
+	generates: {
+		"generated/": {
+			preset: "client",
+			plugins: [],
+			config: {
+				useTypeImports: true,
+				scalars: {
+					uuid: "string",
+				},
+			},
+			presetConfig: {
+				// recommended by graphql-codegen since it's not actually a react hook
+				// https://the-guild.dev/graphql/codegen/plugins/presets/preset-client#the-usefragment-helper
+				fragmentMasking: { unmaskFunctionName: "getFragmentData" },
+			},
+		},
+	},
 };
 
 export default config;
